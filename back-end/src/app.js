3501390049d95be8a2ce705d7b86cca1175fe1e0
dovran_masharipov <120339031+Dovran01@users.noreply.github.com>
2023-10-31 19:28:11 -0400

    new

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -19,9 +19,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Welcome to the home route!');
-});
 app.use("/reservations", reservationsRouter);
 app.use("/tables", tablesRouter);
 
